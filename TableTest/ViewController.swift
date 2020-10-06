7a4236ef12bd65683748c00f0d0aee5bd35b35cd
Alexander von Below <alexander.von_below@telekom.de>
2020-10-07 00:00:55 +0200

    Initial Commit

---
+++ TableTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableTest
+//
+//  Created by Alexander v. Below on 07.10.20.
+//  Copyright © 2020 Alexander von Below. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
